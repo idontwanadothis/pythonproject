af0bf68e4439d28b3fbcb5b50dc41562a19594d5
ITSC <ITSC@gmail.com>
2022-11-26 12:49:47 +0900

    11/26

---
+++ day3/section04/EX04-3-comparison.py
@@ -0,0 +1,11 @@
+'''
+관계 연산자
+    2개 항을 비교하여 그 결과를 논리((bool) 자료형으로 전환
+'''
+a = 15
+print('{} > 10 : {}'.format(a, a > 10))
+print('{} > 10 : {}'.format(a, a < 10))
+print('{} >= 10 : {}'.format(a, a >= 10))
+print('{} <= 10 : {}'.format(a, a <= 10))
+print('{} == 10 : {}'.format(a, a == 10))
+print('{} != 10 : {}'.format(a, a != 10))
---
+++ day3/section3/EX03-5-casting.py
@@ -0,0 +1,54 @@
+'''
+casting
+    변수에 유형을 저장하려는 경우 캐스팅으로 가능
+'''
+#정수형
+x = int(1)
+print(x)
+y = float(2.8)
+print(y)
+z = int("3")
+print(z)
+print(type(z))
+
+print(y+z)
+
+#실수형
+x = float(1)
+print(x)
+z= float("3")
+print(z)
+
+# 문자형
+x= str(1)
+y= str(2)
+print(x)
+print(x+y)
+
+a= ord('A')
+print("a의 값은?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
